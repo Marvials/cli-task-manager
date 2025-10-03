484323b8a8f47cab7beea0c02532437d40ec3827
Marcos Vinicius <marcosgaba7@gmail.com>
2025-10-03 11:57:45 -0300

    add Task struct

---
+++ internal/model/task.go
@@ -0,0 +1,11 @@
+package model
+
+import "time"
+
+type task struct {
+	ID          uint64    `json:"id"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
