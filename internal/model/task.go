2df6635e064199794e845989a071a32a242a1dc4
Marcos Vinicius <marcosgaba7@gmail.com>
2025-12-03 18:58:55 -0300

    docs(model): add godoc to ParseTaskStatus method

--- internal/model/task.go
+++ internal/model/task.go
@@ -21,6 +21,9 @@ type Task struct {
 	CreateAt    time.Time
 }
 
+// ParseTaskStatus validates and converts a raw string into a strongly typed TaskStatus.
+// It performs a case-insensitive comparison against valid statuses ("To do", "Doing", "Done").
+// Returns an error if the provided strings does not match any valid status.
 func ParseTaskStatus(statusRaw string) (TaskStatus, error) {
 	switch {
 	case strings.EqualFold(statusRaw, string(TASK_STATUS_DO)):
